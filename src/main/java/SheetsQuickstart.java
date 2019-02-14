e87489f2579a106eab8a2175dfc58c9913a631ef
joshuajenkins109 <joje3221@colorado.edu>
2019-02-13 18:00:17 -0700

    debug trying(8)

--- src/main/java/SheetsQuickstart.java
+++ src/main/java/SheetsQuickstart.java
@@ -98,7 +98,7 @@ public class SheetsQuickstart {
                 ui.setAction(0);
                 sheet.createDuckList(sheet.sortMasterStudentList(schedule.getMasterList(), ui.getDay()), ui.getDay());
                 List<String> printer = schedule.printToString(trying, ui.getDay());
-                rui.setMorningDuckList(Integer.toString(trying.size()));
+                rui.setMorningDuckList(Integer.toString(trying.get(8).get(3).size()));
                 rui.setMidDuckList(printer.get(1));
                 rui.setDinnerDuckList(printer.get(2));
                 frame.setVisible(false);
